280eac87c639694edc26af0010cd7bab0277808f
Ryan M. Hoffman <rmh.software@gmail.com>
2016-12-31 18:45:39 -0500

    feature: bottom navigation bar

--- app/src/main/java/com/software/rmh/walkthroughthebible/Controllers/MainActivity.java
+++ app/src/main/java/com/software/rmh/walkthroughthebible/Controllers/MainActivity.java
@@ -1,7 +1,10 @@
 package com.software.rmh.walkthroughthebible.Controllers;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.software.rmh.walkthroughthebible.R;
 
@@ -14,9 +17,33 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		initViews();
+
 	}
 
 	private void initViews(){
-
+		BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationBar);
+		bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+			@Override
+			public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+				switch(item.getItemId()){
+					case R.id.books:
+						// Books Fragment
+						break;
+					case R.id.glossary:
+						// Glossary Fragment
+						break;
+					case R.id.tour:
+						// Tour Fragment
+						break;
+					case R.id.bibles:
+						// Bibles Fragment
+						break;
+					case R.id.about:
+						// About Fragment
+						break;
+				}
+				return false;
+			}
+		});
 	}
 }
