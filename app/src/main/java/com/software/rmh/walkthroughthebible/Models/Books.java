f21d5c5ecf05c2f92b5e565c3dd32a80bf6f55cb
Ryan M. Hoffman <rmh.software@gmail.com>
2017-02-02 23:38:33 -0500

    update: add Models

---
+++ app/src/main/java/com/software/rmh/walkthroughthebible/Models/Books.java
@@ -0,0 +1,17 @@
+package com.software.rmh.walkthroughthebible.Models;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * Created by rhoffman on 2/2/17.
+ *
+ * Just a map of all books of the Bible with how many chapters each has.
+ */
+
+public enum Books {
+	;
+
+	private static final Map<String, Integer> map = new HashMap<>();
+
+
+}
