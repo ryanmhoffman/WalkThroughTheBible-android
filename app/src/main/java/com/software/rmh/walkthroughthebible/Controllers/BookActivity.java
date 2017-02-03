5fc3c79a24e68ca694b584d50a974318a70e0715
Ryan M. Hoffman <rmh.software@gmail.com>
2017-02-02 23:11:33 -0500

    update: add maps to chapter

--- app/src/main/java/com/software/rmh/walkthroughthebible/Controllers/BookActivity.java
+++ app/src/main/java/com/software/rmh/walkthroughthebible/Controllers/BookActivity.java
@@ -4,12 +4,18 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.software.rmh.walkthroughthebible.R;
 import com.software.rmh.walkthroughthebible.Views.ChapterFragment;
 
 import java.util.ArrayList;
 
-public class BookActivity extends AppCompatActivity {
+public class BookActivity extends AppCompatActivity implements OnMapReadyCallback{
 
 	private ArrayList<String> books = new ArrayList<>();
 	private int position;
@@ -36,8 +42,15 @@ public class BookActivity extends AppCompatActivity {
 		// Set the ActionBar title to the name of the book.
 		if(getSupportActionBar() != null) getSupportActionBar().setTitle(books.get(position));
 
+		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+		mapFragment.getMapAsync(this);
 	}
 
 
-
+	@Override
+	public void onMapReady(GoogleMap googleMap) {
+		LatLng canton = new LatLng(40.7989, 81.3784);
+		googleMap.addMarker(new MarkerOptions().position(canton).title("Canton, OH"));
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(canton));
+	}
 }
